e5f33f7ce071b8485bf23aaa4ab01f76cfa2c765
Harold Manuel <aquinoharold460@gmail.com>
2023-07-14 11:25:31 -0400

    Funcionalidad desplegar tareas

--- app.js
+++ app.js
@@ -21,8 +21,32 @@ const agregarTarea = () => {
     /* Se agrega el objeto al arreglo */
     tareas.push(newTarea);
     if (newTarea) {
-      /* Llamar a la funcion recorrer */
+      desplegarTareas();
     }
     document.querySelector(".input").value = "";
   }
+};
+
+const desplegarTareas = () => {
+  /* Creamos un elemento ul y eliminamos el contenido de este */
+  const ul = document.querySelector("ul");
+  ul.textContent = "";
+  tareas.forEach((tarea) => {
+    /* Recorremoos todas las tareas del arreglo */
+    /* Para cada tarea creamos un elemento li con un atributo id, esto para eliminar y cambiar el estado mas adelante */
+    const li = document.createElement("li");
+    li.setAttribute("data-id", tarea.id);
+    if (tarea.completado) {
+      /* Si la tarea esta completada, le colocamos una clase */
+      li.classList.add("completado");
+    }
+    /* Rellenamos el contenido del li */
+    li.innerHTML = `<p>${tarea.nombre}</p><button class="delete"><i class="fa-solid fa-trash"></i></button>`;
+    const deleteBtn = li.querySelector(".delete");
+    const cambiarEstado = li.querySelector("p");
+    /* NOTA: Cuando las funciones de eliminar y cambiar el estado esten completadas, llamarlas aqui */
+    /* cambiarEstado.addEventListener("click", estado)
+    deleteBtn.addEventListener("click", eliminarTarea) */
+    ul.appendChild(li);
+  });
 };
